287a32a9bdb0d93bd245e0b10055e231a88890bd
efactor <efactoronline@gmail.com>
2020-05-15 11:33:31 +0100

    crm fist commit

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/adikasundayking/Documents/CODE/Python/crm/crm_project/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
